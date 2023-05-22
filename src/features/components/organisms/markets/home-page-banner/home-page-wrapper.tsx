a405ee30e07483eaf0d0c0262812bcef4ed03107
Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
2023-05-22 16:23:01 +0300

    fix: banner placement

--- src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
+++ src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import ImageBanner from './image-banner'
 import BannerText from './banner-text'
 import { HomepageBannerType } from './types'
-import { item_container, flex_container } from './styles.module.scss'
+import { flex_container, item_container } from './styles.module.scss'
 import Container from 'features/components/atoms/container'
 import Flex from 'features/components/atoms/flex-box'
 
@@ -12,15 +11,11 @@ interface HomepageBannerProps {
 
 const HomePageWrapper = ({ item }: HomepageBannerProps) => {
     return (
-        <Container.Fixed as="section" bgcolor="white" className={item_container}>
-            <Flex.Box
-                container="fluid"
-                className={flex_container}
-                direction="col-reverse"
-                lg={{ direction: 'row' }}
-            >
+        <Container.Fixed as="section" bgcolor="white" className={item_container} pt="10x">
+            <Flex.Box className={flex_container} direction="col-reverse" lg={{ direction: 'row' }}>
                 <BannerText item={item} />
-                <ImageBanner item={item} />
+                {/*change it*/}
+                <Flex.Box md={{ ml: '40x' }}>{item.image}</Flex.Box>
             </Flex.Box>
         </Container.Fixed>
     )
