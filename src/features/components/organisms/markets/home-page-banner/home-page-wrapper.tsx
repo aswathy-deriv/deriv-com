9b1ccd211d2f6e3a0b89cbed66c171b9fbf6b643
Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
2023-05-23 07:37:47 +0300

    chore: make text align with image

--- src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
+++ src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import BannerText from './banner-text'
 import { HomepageBannerType } from './types'
-import { flex_container, item_container } from './styles.module.scss'
-import Container from 'features/components/atoms/container'
+import { flex_container } from './styles.module.scss'
 import Flex from 'features/components/atoms/flex-box'
 
 interface HomepageBannerProps {
@@ -11,13 +10,16 @@ interface HomepageBannerProps {
 
 const HomePageWrapper = ({ item }: HomepageBannerProps) => {
     return (
-        <Container.Fixed as="section" bgcolor="white" className={item_container} pt="10x">
-            <Flex.Box className={flex_container} direction="col-reverse" lg={{ direction: 'row' }}>
-                <BannerText item={item} />
-                {/*change it*/}
-                <Flex.Box md={{ ml: '40x' }}>{item.image}</Flex.Box>
-            </Flex.Box>
-        </Container.Fixed>
+        <Flex.Box
+            className={flex_container}
+            direction="col-reverse"
+            justify="center"
+            md={{ direction: 'row', justify: 'end', align: 'start' }}
+        >
+            <BannerText item={item} />
+            {/*change it*/}
+            <Flex.Box md={{ ml: '40x' }}>{item.image}</Flex.Box>
+        </Flex.Box>
     )
 }
 
